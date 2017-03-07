a5657c6e68f839c48365609b34b97ae85783ede6
Jdasi <joseph2.dasilva@live.uwe.ac.uk>
2017-03-07 11:37:02 +0000

    Added temp FileIO test code; added movable camera; added textures

---
+++ Assets/Scripts/CameraControls.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour
+{
+    public float move_speed = 3.0f;
+    public float scroll_speed = 50.0f;
+    public float shift_modifier = 3.0f;
+
+    private Camera cam;
+    private float current_modifier = 1.0f;
+    private float original_zoom;
+
+	void Start()
+    {
+		cam = GetComponent<Camera>();
+        original_zoom = cam.orthographicSize;
+	}
+	
+	void Update()
+    {
+        handle_modifier();
+        handle_movement();
+        handle_zoom();
+	}
+
+    void handle_modifier()
+    {
+        if (Input.GetButton("CameraSpeedModifier"))
+        {
+            current_modifier = shift_modifier;
+        }
+        else
+        {
+            current_modifier = 1.0f;
+        }
+    }
+
+    void handle_movement()
+    {
+        Vector3 temp = transform.position;
+
+        temp.x += Input.GetAxis("Horizontal") * move_speed * Time.deltaTime * current_modifier;
+        temp.y += Input.GetAxis("Vertical") * move_speed * Time.deltaTime * current_modifier;
+
+        transform.position = temp;
+    }
+
+    void handle_zoom()
+    {
+        cam.orthographicSize -= Input.GetAxis("MouseScroll") * scroll_speed * Time.deltaTime * current_modifier;
+        cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, 1, 10);
+
+        if (Input.GetButtonDown("ResetCameraZoom"))
+        {
+            cam.orthographicSize = original_zoom;
+        }
+    }
+}
--- Assets/Scripts/FileIO.cs
+++ Assets/Scripts/FileIO.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using LitJson;
+using System.IO;
 
 public class FileIO : MonoBehaviour
 {
+    public JSWLevel level = new JSWLevel(new int[] { 0, 0, 1, 0, 2, 3, 4 });
+    JsonData level_data;
+
 	void Start()
     {
-		
+		level_data = JsonMapper.ToJson(level);
+        File.WriteAllText(Application.dataPath + "/Level.json", level_data.ToString());
 	}
-	
-	void Update()
+}
+
+public class JSWLevel
+{
+    public int [] tiles;
+
+    public JSWLevel(int[] tiles)
     {
-		
-	}
+        this.tiles = tiles;
+    }
 }
